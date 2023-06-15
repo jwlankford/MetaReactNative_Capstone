abee27e23390e3177fc7c900ed2bf1aac599a081
Jeremy Lankford <jwlankford@gmail.com>
2023-06-15 09:31:06 -0400

    Exercise: Create a login page

--- App.js
+++ App.js
@@ -4,14 +4,17 @@ import { View, StyleSheet } from 'react-native';
 import LlHeader from './components/LlHeader/LlHeader';
 import LlFooter from './components/LlFooter/LlFooter';
 import MenuItems from './components/MenuItems';
+import LoginScreen from './components/LoginScreen';
 
 export default function App() {
   return (
     <>
       <View style={styles.container}>
         <LlHeader />
-        <MenuItems />
-        <LlFooter />
+        <LoginScreen />
+      </View>
+      <View style={styles.footerContainer}>
+        <LittleLemonFooter />
       </View>
     </>
   );
